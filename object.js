5b833d0c862558624945626a274e71a177b1205b
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-23 10:45:58 +0600

    v1 is here

---
+++ object.js
@@ -0,0 +1,17 @@
+// p:1
+const obj = {};
+obj.name = 'sayed';
+obj.age = 22;
+obj.discition = 'pending';
+obj.course = function coursePersent() {
+    console.log('i will completed my  course as soon as possible ');
+}
+// console.log(obj);
+// p:02
+const player = {};
+player.name ='sakib';
+player.level = 'high';
+player.bat = function batdetails() {
+    console.log('bTaiing is wonderfull');
+}
+console.log(player);
